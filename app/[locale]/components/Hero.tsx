a000934a46324c83c91b855b12c95833ac12d07a
MersalHussin <mersalhussin@gmai.com>
2026-02-25 14:51:29 +0200

    Hero Responsive

--- app/[locale]/components/Hero.tsx
+++ app/[locale]/components/Hero.tsx
@@ -7,15 +7,17 @@ const Hero = () => {
     
     return (
         <section className="hero__section bg-main">
-            <div className="min-h-[calc(100vh-100px)] w-full bg-main flex justify-center items-center flex-col px-4 py-8">
-                <h1 className="font-bold text-white text-center text-3xl md:text-4xl lg:text-5xl max-w-110 leading-tight md:leading-relaxed">
+            <div className="container__hero min-h-[calc(100vh-100px)] w-full bg-main flex justify-center items-center flex-col px-4 py-8">
+                <div className="flex flex-col justify-center items-center scale-80 md:scale-100 lg:scale-120">
+                <h1 className="font-bold text-white text-center text-5xl  max-w-110 leading-tight md:leading-relaxed">
                     {t('title')} <span className="hero__highlight">{t('highlight')}</span>
                 </h1>
                 <Image src="/assets/Line-Hero.svg" alt="Title line" height={200} width={200} className="mt-5 md:w-[320px]" />
-                <p className="text-white font-bold max-w-[400px] md:max-w-[400px] text-center text-sm md:text-base mt-4">{t('description')}</p>
-                <div className="btns flex flex-col md:flex-row gap-3 md:gap-4 mt-6 w-full md:w-auto">
+                <p className="text-white font-bold max-w-[400px] text-center text-sm md:text-base mt-4">{t('description')}</p>
+                <div className="btns flex gap-2 mt-3 ">
                     <Link href="#" className="bg-white hover:bg-third transition-all px-6 py-3 text-main font-bold rounded text-center">{t('contactBtn')}</Link>
                     <Link href="#" className="border-2 border-white text-white hover:bg-white hover:text-main transition-all px-6 py-3 font-bold rounded text-center">{t('plansBtn')}</Link>
+                </div>
                 </div>
             </div>
         </section>
